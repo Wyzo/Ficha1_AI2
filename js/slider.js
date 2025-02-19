7622483e8fa7ba650dc02f683fea516d7f991fd0
Francisco Duarte <franciscoperes312@gmail.com>
2025-02-19 14:40:39 +0000

    First commit

---
+++ js/slider.js
@@ -0,0 +1,48 @@
+function iniciarSliderAutomatico() {
+    const div_centro = document.getElementById("centro_index");
+    const titulo_filme = document.getElementById("titulo_filme");
+    const tipo_filme = document.getElementById("tipo_filme");
+
+    const imagens = 
+    [   "assets/img/filme5.png", 
+        "assets/img/filme2.jpg",
+        "assets/img/filme3.jpg",
+        "assets/img/filme4.jpg",
+        "assets/img/filme5.jpg",
+        "assets/img/filme6.jpeg"];
+
+    const titulos = 
+    [   "Top Gun - Maverick",
+        "Blade Runner 2045",
+        "O criador",
+        "Rogue One - Uma história star wars",
+        "O Mandalorian",
+        "A Víuva Negra"
+    ];
+
+    const tipos = 
+    [   "Ação, Drama",
+        "História, Drama",
+        "Ficção-ciêntifica",
+        "Ficção-ciêntifica",
+        "Ação",
+        "Ação"
+    ];
+
+    let indiceAtual = 0;
+
+    function trocarImagem() {
+        const novaImagem = imagens[indiceAtual];
+        titulo_filme.innerHTML = titulos[indiceAtual];
+        tipo_filme.innerHTML = tipos[indiceAtual];
+        div_centro.style.backgroundImage = `linear-gradient(357deg, rgba(30,30,30,1) 0%, rgba(34,34,35,1) 17%, rgba(4,0,65,0) 100%), url(/${novaImagem})`;
+        indiceAtual = (indiceAtual + 1) % imagens.length; 
+    }
+
+    trocarImagem(); 
+    setInterval(trocarImagem, 5000);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    iniciarSliderAutomatico();
+});
